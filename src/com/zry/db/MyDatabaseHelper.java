95f55ffe5c6e8e23b5116e2f6a8b3877e5091d95
zryzhangruyi <731134543@qq.com>
2016-09-18 07:55:28 +0800

    11

--- src/com/zry/db/MyDatabaseHelper.java
+++ src/com/zry/db/MyDatabaseHelper.java
@@ -2,24 +2,26 @@ package com.zry.db;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
 /**
  * 数据库辅助类
+ * 
  * @author zry
  *
  */
-public class MyDatabaseHelper extends SQLiteOpenHelper{
-	private static final String USER="create table user (name text,pw text)";
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+	private static final String USER = "create table user (name text,pw text)";
+
 	public MyDatabaseHelper(Context context, String name, int version) {
-		super(context, name,null, version);
+		super(context, name, null, version);
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(USER);
-		Log.d("ryzh", "创建表成功");
+		Log.d("ryzh", "创建数据库");
 	}
 
 	@Override
